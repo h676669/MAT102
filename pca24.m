5d86d427b688951df0d0b65d81177f57089bd811
h676669 <142335301+h676669@users.noreply.github.com>
2024-10-01 09:46:28 +0200

    hei

--- pca24.m
+++ pca24.m
@@ -1,1 +1,16 @@
 load('pcaeksempel.mat')
+
+[n, m] = size(X);
+
+meanX=mean(X);
+stdX=std(X);
+
+for j=1:m
+    X(:,j)=X(:,j)-meanX(j);
+
+    if stdX(j)~=0
+        X(:,j) = X(:,j)/stdX(j);
+    end
+end
+
+Xnorm=X;
