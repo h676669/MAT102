61d27238911651480e23b8913bff9533b3a7b30f
h676669 <676669@stud.hvl.no>
2024-10-17 21:14:08 +0200

    hei

--- eks1.m
+++
@@ -1,8 +0,0 @@
-y=-5:0.25:5;
-x=-5:0.25:5;
-[x,y]=meshgrid(x,y);
-f=x.^2+y.^2;
-surf(x,y,f);
-xlabel('x');
-ylabel('y');
-zlabel('z');
--- untitled.m
+++
@@ -1,8 +0,0 @@
-format compact
-a=4;
-b=6;
-clc
-
-I = [5 8 9 3 1];
-I;
-
